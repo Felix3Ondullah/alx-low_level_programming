9a605a6c788e474c6669a1c7921f491c22b74644
Felix3Ondullah <felixondullah@gmail.com>
2022-03-15 06:45:45 -0700

    3 slower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
+ */
+
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-	_putchar('\n');
+	return (c >= 'a' && c <= 'z');
 }
