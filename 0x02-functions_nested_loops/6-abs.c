1e54c2e34b4902d12138f4a51c35be1769f0b81e
Felix3Ondullah <felixondullah@gmail.com>
2022-03-15 06:56:45 -0700

    5 signs and 6 abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints te absolute value
+ * @n: input parameter
+ * Return: Always 0.
+ */
 
 int _abs(int n)
-	{
-	if (n >= 0)
-	{
-	return (n);
-	}
-	else
-	{
-	return (n * -1);
-	}
+{
+	return (abs(n));
 }
+
